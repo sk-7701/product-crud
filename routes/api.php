464e5842d678ceedd9351837c4315769c960b706
sk-7701 <ss652659@gmail.com>
2024-11-10 23:31:56 +0530

    Added: routes for product resources

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,11 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->group( function () {
+    Route::resource('products', ProductController::class);
+    
+});
 
 Route::post('register',[AuthController::class,'register']);
 Route::post('login',[AuthController::class,'login']);
