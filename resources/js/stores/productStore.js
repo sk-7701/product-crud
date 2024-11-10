c98896ed7f81e99f19905987eca80cecd47907b1
sk-7701 <ss652659@gmail.com>
2024-11-10 19:30:36 +0530

    Fixed:routing issue after user is registered

--- resources/js/stores/productStore.js
+++ resources/js/stores/productStore.js
@@ -1,6 +1,5 @@
 import { defineStore } from 'pinia';
 import axios from 'axios';
-import router from '../routes';
 import { useToast } from 'vue-toastification';
 export const useProductStore = defineStore('product',{
     state : () => ({
@@ -28,10 +27,9 @@ export const useProductStore = defineStore('product',{
 
             const toast = useToast(); 
             axios.post('http://localhost:8000/api/register', this.form_data).then((response) => {
-                console.log(response.data)  
                 this.form_data.name = this.form_data.email = this.form_data.password = this.form_data.password_confirmation = null;
-                this.$router.push('/login')
-                toast.success('Account created successfully, now you can login!')
+                toast.success('Account created successfully, now you can login!');
+                this.router.push({name:'login'});
             }).catch((errors) => {
                 this.register_errors = errors.response.data.errors
                 
