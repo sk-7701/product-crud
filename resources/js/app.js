697e139f9a9e5ede19fa3201a6c0062eb70cc00d
sk-7701 <ss652659@gmail.com>
2024-11-13 04:48:43 +0530

    Added: remaining files

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,20 @@
 import './bootstrap'; 
-import { createApp } from 'vue';
+import { createApp, markRaw } from 'vue';
 import App from './App.vue'; 
-import router from './routes'; 
+import router from './routes'
 import { createPinia } from 'pinia';
-
+import Toast from 'vue-toastification';
+import 'vue-toastification/dist/index.css';
 const app = createApp(App);
-
-app.use(router);
+const pinia = createPinia();
+pinia.use(({store}) =>
+{
+    store.router = markRaw(router)
+})
 app.use(pinia);
-
+app.use(router);    
+app.use(Toast, {
+    position: 'top-right',  
+    timeout: 5000,
+  });
 app.mount('#app'); 
